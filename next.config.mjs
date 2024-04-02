e41bf615f27fca0c0ddfeb934851ff883ebc503f
Daniil Utkin <d.utkin@tesonero.com>
2024-04-02 13:20:54 +0300

    add github pages deployment pipeline

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  basePath: "/browser-camera-practice",
+  output: 'export',
   poweredByHeader: false,
 };
 
